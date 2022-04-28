78d8652b7aa5484ff37599b3bf307ddfe8ba72c8
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2022-04-28 10:45:01 +0200

    maj

--- lib/convert_utils.php
+++ lib/convert_utils.php
@@ -6,8 +6,10 @@ function convertCelsius($unit, $temp)
   if ($unit == "fah") {
     $resultat = ($temp * 9 / 5) + 32;
     return $resultat . " °F";
-  } else {
+  } elseif ($unit == "kel") {
     $resultat = $temp + 273.15;
     return $resultat . " °K";
+  } else {
+    return 0;
   }
 }
