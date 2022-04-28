1c0b9237987f432fad36100075eddd6b5e52d235
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2022-04-28 11:21:44 +0200

    to the top

--- convertisseur2.php
+++ convertisseur2.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+if (!$_SESSION['password']) {
+  header('location: http://localhost/TPABRAGAPHP/conn_success.php');
+}
 
 if (isset($_GET['unit']) && isset($_GET['temp']) && !is_null($_GET['temp']) && !empty($_GET['unit'])) {
   include "lib/convert_utils.php";
